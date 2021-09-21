82dbd426b47642c028b62624d4cacb798c249a28
Gonzalo Puñales <lascano.gonzalo.ivan@gmail.com>
2021-09-20 23:01:10 -0300

    Se hace commit del primer ciclo funcional

--- readme.txt
+++ readme.txt
@@ -6,3 +6,15 @@ Admigual Budget Tool
 
 7/9/21 Se hace el primer commit
 
+-------------------------------------------
+Version 15/9/21 Primera version en la que se crea de principio a fin el presupuesto para un solo proyecto.
+
+a futuro las tareas a realizar son:
+
+1.Resetear el formulario de tareas cuando se guarda un producto o se cambia su tipo.
+2.Boton para finalizar el proyecto (resetea todo el formulario menos el nombre y el mail)
+3.Corregir habilitadores e inhabilitadores (para controlar los parametros que se ingresan por imput que sean apropiados)
+4.Agregar limitadores: de proyectos a 3, de productos (x proyecto) a 6
+5.Programar creacion de tarjeta por proyecto.
+6.Mejorar la interfaz.
+
--- script.js
+++ script.js
@@ -1,25 +1,43 @@
-/* probando jquery */
-/* $("document").ready(function(){
-    alert("JQUERY SABE!")
-}); */
 
-console.log($("#userName"));/* diferencia entre este metodo y el de abajo. por que en uno se llama .value y ene el otro .val() */
-console.log(document.getElementById("userName"));
+$(document).ready();
+
+//<pruebas api--------
+
+//const WT_URL = "http://worldtimeapi.org/api/timezone/America/Argentina/Buenos_Aires";
+/* const WT_URL = "http://worldclockapi.com/api/json/utc/now";
+
+fetch(WT_URL)
+    .then(r => r.json())
+    .then(data => data.datetime)
+    .then(strdate => new Date( Date.parse(strdate) ))
+    .then(changeColor); */
+
+
+/* function changeColor(now) {
+    console.log(now);
+    if (now.getHours() < 15) {
+        $('body').css({
+            'background-color': 'blue'
+        })
+    }
+} */
+//</pruebas api-------------
+
 
 /* Definiendo variables */
-let client ="";
+let client;
 let project; 
-let prodType ="";
-let prodTime ="";
+let prodType;
+let prodTime;
 let laborFee = 500;
 const TASKS = ["#design3d", "#production3d", "#render3d", "#body2d", "#face2d", "#character2d", "#evMontaje", "#evColor", "#evEncoding"];
 /* Definiendo Classes que construiran los objetos principales */
 
 class Client {
     constructor(name, mail){
-        this.name = name;/* citar formulario */
-        this.mail = mail;/* citar formulario */
-        this.projects = [];/*arrays con pryectos del formulario citar formulario */
+        this.name = name;
+        this.mail = mail;
+        this.projects = [];
     }
     /* clientLog(this){
         localstorage.setItem("Client", JSON.stringify(this));
@@ -29,33 +47,34 @@ class Client {
     }
 };
 
-/* let p = new Project();
-client.addProject(p); ----ensayo---- */
 
 class Project {
     constructor(name) {
         this.name = name;
         this.products = [];
         this.time = 0;
-        this.cost = this.time * laborFee;
+        this.cost = 0;
     }
     projectLog() {
         localstorage.setItem("Project", JSON.stringify(this));
     }
     addProduct(product){
         this.products.push(product);
+        this.time = this.time + product.time;
+        this.cost = this.time * laborFee;
     }
 }
 
 class Product {
     constructor(type){
         this.type = type;
-        this.subtype = parseInt($('#subType3d').children("option:selected").val()) + parseOrDefault("#seg2d") + parseOrDefault("#minVideo");
-        this.tasks = sumTasksValues(); //suma de los valores chequeados en los boxes
-        this.time = this.tasks * this.subtype;
+        this.multiplier = parseInt($('#subType3d').children("option:selected").val()) + parseOrDefault("#seg2d") + parseOrDefault("#minVideo");
+        this.tasks = createTasks(); //suma de los valores chequeados en los boxes
+        this.time = this.tasks.total * this.multiplier;
     }
-    productLog() {
-        localstorage.setItem("Product", JSON.stringify(this));
+
+    addSelectedTsk(tasklist) {
+        this.tasks.selectedTasks.push(tasklist)
     }
 }
 
@@ -65,16 +84,8 @@ class Product {
 $("#btnNewCl").on("click", createUser);
 $('#btnNewProj').on("click",createProj);
 $('#addProd').on("click",createProd);
-
-/* Seleccion de Tipos para habilitar multiplicadores */
-/* $('#radio3d').on("click", showType($('#subType3d'), $('#tasks3d')));
-$('#radio2d').on("click", showType($('#timer2d'), $('#tasks2d')));
-$('#radioEV').on("click", showType($('#EVTimer'), $('#tasksEv')));  Asi probe la primera vez pero por algun motivo no funcionaba
-si no le agregaba una funcion flecha antes*/
-
-$('#radio3d').change(() => showType('#subType3d', '#tasks3d'));
-$('#radio2d').change(() => showType('#timer2d', '#tasks2d'));
-$('#radioEV').change(() => showType('#EVTimer', '#tasksEv'));
+$('#typeCont input').on('click', newShowType);
+$('#saveUserData').on('click',storeUser);
 
 /* --------------------Agregando Funciones-------------------- */
 
@@ -90,52 +101,145 @@ function createProj(){
     project = new Project($("#projName").val());
     $("#addProd").prop('disabled',false);
     client.addProject(project);
-    console.log(client.projects);
+    console.log(client);
+
 }
 
 function createProd(){
+    prodType = $('#typeCont input:checked').next("label").text();
     let product = new Product(prodType);
-    project.addProduct(product)
-    console.log(project.products);   
+    project.addProduct(product);
+    console.log(client)
 }
 
 /* Mostrador de multiplicadores y tareas */
 
-const ALL = ['#subType3d', '#tasks3d', '#timer2d', '#tasks2d', '#EVTimer', '#tasksEv']; 
+const MULTI = ['#subType3d', '#tasks3d', '#timer2d', '#tasks2d', '#EVTimer', '#tasksEv']; 
 
-function showType(multiplier, tasks) {
-    for (let toHide of ALL) {
+function newShowType() {
+    resetCheckBoxes();
+    resetProyText();
+    resetOption();
+    for (let toHide of MULTI) {
         $(toHide).hide();
     }
-    $(multiplier).show();
-    $(tasks).show();
-    if (multiplier.includes("3d")){
-        prodType = "Asset 3d";
+    let checked = $('#typeCont input:checked').val();
+    if (checked == "0") {
+        $('#subType3d').show();
+        $('#tasks3d').show();
     }
-    else if (multiplier.includes("2d")){
-        prodType = "Animación 2d";
+
+    else if (checked == "1") {
+        $('#timer2d').show();
+        $('#tasks2d').show();
     }
-    else if (multiplier.includes("EV")){
-        prodType = "Edicion de Video";
+
+    else if (checked == "2") {
+        $('#EVTimer').show();
+        $('#tasksEv').show();
     }
-    console.log(prodType); 
 }
 
-function sumTasksValues() {
+/* Manejo de las tareas del Producto -------------*/
+
+function createTasks() {
     let ret = 0;
+    let checkedTasks = []
     for (let task of TASKS) {
         if ($(task).is(":checked")) {
             ret = ret + parseOrDefault(task);
+            checkedTasks.push($(task).next("label").text());
         }
     }
-    return ret;
+    return {total: ret, chosenTasks: checkedTasks};
 }
 
-function parseOrDefault(algo) {
-    if ($(algo).val() == "") {
+function parseOrDefault(task) {
+    if ($(task).val() == "") {
         return 0;
     } 
-    return parseInt($(algo).val());
+    return parseInt($(task).val());
 }
 
 
+//LIMPIADORES DE FORMULARIOS-----------------------
+
+function resetForm() {
+    resetCheckBoxes();
+    resetTextBoxes();
+    resetRadios();
+    resetOption();    
+}
+
+function resetCheckBoxes() {
+    $('input[type=checkbox]').prop('checked',false);
+}
+
+function resetTextBoxes() {
+    $('input[type=text]').val("");
+}
+
+function resetProyText() {
+    $('.regProyecto input[type=text]').val("");
+}
+
+function resetOption(){
+    $('#subType3d').val("0")
+}
+
+function resetRadios(){
+    $('#typeCont input').prop('checked', false);
+}
+
+//-----------------------------------------------------
+
+/* Storage, almacenamiento y AJAX */
+
+function storeUser() {
+    console.log(client);
+    localStorage.setItem(client.mail, JSON.stringify(client));
+    displayCard();
+    resetForm();
+}
+
+function displayCard() {
+    $('#usuario').text(` ${client.name}`);
+    $('.card__title').text(`Tu proyecto ${project.name} llevaria unas ${project.time}hs de trabajo y costaria aproximadamente $${project.cost}.`)
+    let showCard = $('#card-1');
+    showCard.show();
+    let proyCard = $('.card');
+    proyCard.css({
+        'top':'-40rem',
+        'opacity':'0',
+        'transform':'scale(1.2)',
+    }).animate({
+        'opacity':'1',
+    }, "fast");
+    proyCard.click(()=>{
+        proyCard.animate({'top':'0',},"fast");
+        proyCard.css({'transform':'scale(1)',});
+    });
+
+
+}
+
+const CLIENTURL = 'https://jsonplaceholder.typicode.com/posts' //simulacion de destino del post (no me dejaba hacerlo con un archivo local, error CORS 405)
+
+$('#saveUserData').click(() => {
+    $.post(CLIENTURL, client,(respuesta, estado) => {
+        if (estado === "success") {
+            console.log("guardado");
+        }
+    }
+    );
+}
+)
+
+/*------------------------------------------------------------------- Notas de tareas a realizar
+
+1.Resetear el formulario de tareas cuando se guarda un producto o se cambia su tipo.
+2.Boton para finalizar el proyecto (resetea todo el formulario menos el nombre y el mail)
+3.Corregir habilitadores e inhabilitadores
+4.Agregar limitadores: de proyectos a 3, de productos a 6
+5.Programar creacion de tarjeta por proyecto.
+6.Mejorar la interfaz: Animaciones*/
